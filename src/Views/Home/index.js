116e9eaaccc4b4ec39d0da7dfa6bcbd99565ce3c
Tim Phan <tim.phan@digitalent.se>
2019-11-26 13:59:32 +0100

    Added axios as dependency and simple fetch from omdbapi

--- src/Views/Home/index.js
+++ src/Views/Home/index.js
@@ -1,6 +1,13 @@
 import React, { Component } from "react";
-
+import axios from "axios";
+import config from "../../api/config.json";
 class HomePage extends Component {
+  async componentDidMount() {
+    const fetchedData = await axios.get(
+      `${config.baseUrl}/?apikey=${config.access_key}&t="Drive"`
+    );
+    console.log(fetchedData);
+  }
   render() {
     return (
       <div>
